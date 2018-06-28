cd5bc8024d780f356e94b45b6167b8f2581f7f90
HammadaPlayz <40673859+HammadaPlayz@users.noreply.github.com>
2018-06-28 22:37:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "-"
+var prefix = "#"
 
 client.on('ready', () => {
   console.log('╔[════════════════════════════════════]╗');
@@ -18,7 +18,94 @@ client.on('ready', () => {
 
 
 client.on('ready', () => {
-     client.user.setActivity("-help",{type: 'WATCHING'})
+     client.user.setActivity("#help",{type: 'WATCHING'})
 });
 
+  client.on('message', message => {
+    if (message.content === "#server") {
+        if (!message.channel.guild) return;
+        const millis = new Date().getTime() - message.guild.createdAt.getTime();
+        const now = new Date();
+
+        const days = millis / 1000 / 60 / 60 / 24;
+        let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
+        var embed = new Discord.RichEmbed()
+		.setAuthor(message.guild.name, message.guild.iconURL)
+		.addField("**server Owner**","**"+ message.guild.owner + "**", true)
+		 .addField("**Server ID**", "**" + message.guild.id + "**", true)
+		 .addField("**Server Location**", "**" + message.guild.region + "**", true)
+            .addField('**Server Text Channels**', `**[ ${message.guild.channels.filter(m => m.type === 'text').size} ] Channel **`, true)
+            .addField("**Server Voice Channels**", ` ** [ ${message.guild.channels.filter(m => m.type === 'voice').size} ] Channel ** `, true)
+            .addField("**Date created**", ` ** [ ${days.toFixed(0)} ] ** Day `, true)
+            .addField("**Roles**", `**[${message.guild.roles.size}]** Role `, true)
+       .addField("Members", `
+**${message.guild.memberCount}**`)
+            .setThumbnail(message.guild.iconURL)
+            .setColor('RANDOM')
+        message.channel.sendEmbed(embed)
+
+    
+});
+client.on('message', message => {
+  if (message.guild) {
+ let embed = new Discord.RichEmbed()
+  let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+  if (!args[1]) {
+message.channel.send("**#bc <message>**");
+return;
+
+      message.guild.members.forEach(m => {
+ if(!message.member.hasPermission('ADMINISTRATOR')) return;
+          var bc = new Discord.RichEmbed()
+          .setAuthor(message.author.username, message.author.avatarURL)
+          .addField(' The server', `${message.guild.name}`, true)
+          .addField(' who sended the messege ', `${message.author.username}!${message.author.discriminator}`, true)
+          .addField(' the messege ', args)
+          .setThumbnail(message.guild.iconURL)
+          .setColor('RANDOM')
+          m.send(`${m}`,{embed: bc});
+      });
+      const Hamada = new Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setTitle('✅| the messege is loading ')
+      .addBlankField(true)
+      .addField('♨| i got sended to  ', message.guild.memberCount , true)
+      .addField('📝| the message ', args)
+      .setColor('RANDOM')
+      message.channel.sendEmbed(embed);
+  }
+  } else {
+      return;
+  
+});
+client.on('message', message => {
+     if(message.content.startsWith(prefix + "bc")) {
+ let args = message.content.split(" ").slice(1);
+
+    var user = message.mentions.users.first();
+    var reason = args.slice(1).join(' ');
+    const embed = new Discord.RichEmbed()
+        .setColor(0xFFB200)
+        .setTimestamp();
+
+    if (!user) {
+        embed.addField("DM A Person", `Who are you going to DM ${message.author.tag}?`)
+            .setFooter(`lol why did i add dis again?`);
+        return message.channel.send({embed});
+    } if (!reason) {
+        embed.addField("DM A Person", `What are you going to say to ${user.tag}?`)
+        return message.channel.send({embed});
+    
+    embed.addField("DM A Person", `Successfully sent a DM to ${user.tag}!`)
+        .setFooter(`lol.`);
+    message.channel.send({embed});
+    const embed1 = new Discord.RichEmbed()
+        .setColor(0xFFB200)
+        .setTimestamp()
+        .addField("You have received mail! :mailbox_with_mail:", `**${reason}**`)
+        .setFooter(`Sent by ${message.author.tag}.`);
+    user.send({embed: embed1});
+
+});
 client.login(process.env.BOT_TOKEN);
