e1f47e2f8092b8cdd3fa6850725f9378a862c752
HammadaPlayz <40673859+HammadaPlayz@users.noreply.github.com>
2018-06-28 22:24:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,22 +17,6 @@ client.on('ready', () => {
   console.log(`servers! [ " ${client.guilds.size} " ]`);
   console.log('')
   console.log('╚[════════════════════════════════════]╝')
-});
-          
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "!say") {
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  };
-});
 
 
 
