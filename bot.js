2b1a0c094d688e25bfd953fdc722040efa1dcc33
HammadaPlayz <40673859+HammadaPlayz@users.noreply.github.com>
2018-06-28 22:17:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,58 +35,7 @@ client.on('message', message => {
 });
 
 
-  client.on('message', message => {
-    if (message.content === "!server") {
-        if (!message.channel.guild) return;
-        const millis = new Date().getTime() - message.guild.createdAt.getTime();
-        const now = new Date();
 
 
-        const days = millis / 1000 / 60 / 60 / 24;
-        let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
-        var embed = new Discord.RichEmbed()
-            .setAuthor(message.guild.name, message.guild.iconURL)
-            .addField("**Server ID**", "**" + message.guild.id + "**", true)
-            .addField("**Server Owner**", "**" + message.guild.owner + "**", true)
-            .addField("**Server Location**", "**" + message.guild.region + "**", true)
-            .addField('**Server Text Channels**', `**[ ${message.guild.channels.filter(m => m.type === 'text').size} ] Channel **`, true)
-            .addField("**Server Voice Channels**", ` ** [ ${message.guild.channels.filter(m => m.type === 'voice').size} ] Channel ** `, true)
-            .addField("**Date created**", ` ** [ ${days.toFixed(0)} ] ** Day `, true)
-            .addField("**Roles**", `**[${message.guild.roles.size}]** Role `, true)
-
-        .addField("Members", `
-**${message.guild.memberCount}**`)
-            .setThumbnail(message.guild.iconURL)
-            .setColor('RANDOM')
-        message.channel.sendEmbed(embed)
-
-    };
-});
-
-
-
-client.on('message', message => {
-if (message.content.startsWith("!kick")) {
-    var mention = message.mentions.member.first();
-    if(!mention) return message.channel.send("u want to ping who u want to kick");
-
-    mention.kick("By: " + message.author.tag);
-    
-    message.channel.send("got kicked : " + mention.tag);
-};
-});
-
-
-
-client.on('message', message => {
-if (message.content.startsWith("!ban")) {
-    var mention = message.mentions.members.first();
-    if(!mention) return message.channel.send("u want to ping who u want to ban ;D");
-
-    mention.ban("By: " + message.author.tag);
-    
-    message.channel.send("got banned : " + mention.tag);
-};
-});
 
 client.login(process.env.BOT_TOKEN);
