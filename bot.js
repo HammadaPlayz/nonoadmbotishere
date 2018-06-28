a6ff95e2cff1aad9db2a5edc9deebd9b7f0fa948
HammadaPlayz <40673859+HammadaPlayz@users.noreply.github.com>
2018-06-28 21:26:07 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "-"
+
+client.on('ready', () => {
+  console.log('╔[════════════════════════════════════]╗');
+  console.log('')
+  console.log('            ╔[════════════]╗')
+  console.log('              Client Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log('╚[════════════════════════════════════]╝')
+});
+client.login('process.env.BOT_TOKEN');
