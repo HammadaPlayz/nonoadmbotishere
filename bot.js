d51d827942d05b8d646cb62e92a8b39492b2516f
HammadaPlayz <40673859+HammadaPlayz@users.noreply.github.com>
2018-06-29 00:12:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,41 +20,15 @@ client.on('ready', () => {
 client.on('ready', () => {
      client.user.setActivity("#help",{type: 'WATCHING'})
 });
-
-  client.on('message', message => {
-    if (message.content === "#server") {
-        if (!message.channel.guild) return;
-        const millis = new Date().getTime() - message.guild.createdAt.getTime();
-        const now = new Date();
-
-        const days = millis / 1000 / 60 / 60 / 24;
-        let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
-        var embed = new Discord.RichEmbed()
-		.setAuthor(message.guild.name, message.guild.iconURL)
-		.addField("**server Owner**","**"+ message.guild.owner + "**", true)
-		 .addField("**Server ID**", "**" + message.guild.id + "**", true)
-		 .addField("**Server Location**", "**" + message.guild.region + "**", true)
-            .addField('**Server Text Channels**', `**[ ${message.guild.channels.filter(m => m.type === 'text').size} ] Channel **`, true)
-            .addField("**Server Voice Channels**", ` ** [ ${message.guild.channels.filter(m => m.type === 'voice').size} ] Channel ** `, true)
-            .addField("**Date created**", ` ** [ ${days.toFixed(0)} ] ** Day `, true)
-            .addField("**Roles**", `**[${message.guild.roles.size}]** Role `, true)
-       .addField("Members", `
-**${message.guild.memberCount}**`)
-            .setThumbnail(message.guild.iconURL)
-            .setColor('RANDOM')
-        message.channel.sendEmbed(embed)
-
-    
-});
 client.on('message', message => {
   if (message.guild) {
  let embed = new Discord.RichEmbed()
   let args = message.content.split(' ').slice(1).join(' ');
 if(message.content.split(' ')[0] == prefix + 'bc') {
   if (!args[1]) {
-message.channel.send("**#bc <message>**");
+message.channel.send("**^bc <message>**");
 return;
-
+}
       message.guild.members.forEach(m => {
  if(!message.member.hasPermission('ADMINISTRATOR')) return;
           var bc = new Discord.RichEmbed()
@@ -65,8 +39,8 @@ return;
           .setThumbnail(message.guild.iconURL)
           .setColor('RANDOM')
           m.send(`${m}`,{embed: bc});
-      
-      const Hamada = new Discord.RichEmbed()
+      });
+      const Himo = new Discord.RichEmbed()
       .setAuthor(message.author.username, message.author.avatarURL)
       .setTitle('✅| the messege is loading ')
       .addBlankField(true)
@@ -77,7 +51,6 @@ return;
   }
   } else {
       return;
-  
-
+  }
 });
 client.login(process.env.BOT_TOKEN);
